131284831ae0d53b854e2b22a50ded914364e1f6
nicko858 <ikar2021@hotmail.com>
2018-04-02 15:49:23 +0300

    Исправлена функция arg_parser_init, убран повторяющийся код из '__main__'

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,27 +13,21 @@ def get_most_frequent_words(text, words_count):
     return Counter(only_words).most_common(words_count)
 
 
-def arg_parser_init(value):
+def arg_parser_init():
     script_usage = "lang_frequency.py <path to file> <words_count>"
     parser = argparse.ArgumentParser(description="How to run lang_frequency.py:", usage=script_usage)
-    parser.add_argument("source_text",
-                        help="Specify the path to the source text file")
-    parser.add_argument("words_count", type=int,
+    parser.add_argument("source_text", help="Specify the path to the source text file")
+    parser.add_argument("words_count", type=int, nargs='?', const=1, default=1,
                         help="Specify the words count need to display")
     args = parser.parse_args()
-    if value == "source_text":
-        return args.source_text
-    elif value == "words_count":
-        return args.words_count
+    return args
 
 
 if __name__ == "__main__":
-    source_text = arg_parser_init("source_text")
-    words_count = arg_parser_init("words_count")
     try:
-        loaded_text = load_data(source_text)
+        loaded_text = load_data(arg_parser_init().source_text)
     except IOError:
-        exit("No such file or directory {}".format(source_text))
-    most_freq_words = get_most_frequent_words(loaded_text, words_count)
+        exit("No such file or directory {}".format(arg_parser_init().source_text))
+    most_freq_words = get_most_frequent_words(loaded_text, arg_parser_init().words_count)
     for word, count in most_freq_words:
         print(word, count)
