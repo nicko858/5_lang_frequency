4f93669ade7c29c13aa2e75587e32111db95ebe4
nicko858 <ikar2021@hotmail.com>
2018-04-03 09:41:02 +0300

    The program uses these libs from Python Standart Library # тут надо бы указать список используемых библиотек, а не демонстрировать, как это импортируется в скрипте.

--- lang_frequency.py
+++ lang_frequency.py
@@ -13,11 +13,17 @@ def get_most_frequent_words(text, words_count):
     return Counter(only_words).most_common(words_count)
 
 
-def arg_parser_init():
+def get_args():
     script_usage = "lang_frequency.py <path to file> <words_count>"
-    parser = argparse.ArgumentParser(description="How to run lang_frequency.py:", usage=script_usage)
-    parser.add_argument("source_text", help="Specify the path to the source text file")
-    parser.add_argument("words_count", type=int, nargs='?', const=1, default=1,
+    parser = argparse.ArgumentParser(description="How to run lang_frequency.py:",
+                                     usage=script_usage)
+    parser.add_argument("source_text",
+                        help="Specify the path to the source text file")
+    parser.add_argument("words_count",
+                        type=int,
+                        nargs='?',
+                        const=1,
+                        default=1,
                         help="Specify the words count need to display")
     args = parser.parse_args()
     return args
@@ -25,9 +31,10 @@ def arg_parser_init():
 
 if __name__ == "__main__":
     try:
-        loaded_text = load_data(arg_parser_init().source_text)
+        source_text = get_args().source_text
+        loaded_text = load_data(source_text)
     except IOError:
-        exit("No such file or directory {}".format(arg_parser_init().source_text))
-    most_freq_words = get_most_frequent_words(loaded_text, arg_parser_init().words_count)
+        exit("No such file or directory {}".format(source_text))
+    most_freq_words = get_most_frequent_words(loaded_text, get_args().words_count)
     for word, count in most_freq_words:
-        print (word, count)
+        print(word, count)
