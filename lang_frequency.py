a9578442c068d9311e0d61b8239540b699a7205b
nicko858 <ikar2021@hotmail.com>
2018-04-03 19:53:52 +0300

    Убрал дублирование, поправил форматирование

--- lang_frequency.py
+++ lang_frequency.py
@@ -15,27 +15,28 @@ def get_most_frequent_words(text, words_count):
 
 def get_args():
     script_usage = "lang_frequency.py <path to file> <words_count>"
-    parser = argparse.ArgumentParser(description="How to run lang_frequency.py:",
-                                     usage=script_usage)
-    parser.add_argument("source_text",
-                        help="Specify the path to the source text file")
-    parser.add_argument("words_count",
-                        type=int,
-                        nargs='?',
-                        const=1,
-                        default=1,
-                        help="Specify the words count need to display")
+    parser = argparse.ArgumentParser(description="How to run lang_frequency.py:", usage=script_usage)
+    parser.add_argument("source_text", help="Specify the path to the source text file")
+    parser.add_argument(
+        "words_count",
+        type=int,
+        nargs="?",
+        const=1,
+        default=1,
+        help="Specify the words count need to display"
+    )
     args = parser.parse_args()
     return args
 
 
 if __name__ == "__main__":
+    args = get_args()
+    source_text = args.source_text
+    words_count = args.words_count
     try:
-        source_text = get_args().source_text
         loaded_text = load_data(source_text)
     except IOError:
         exit("No such file or directory {}".format(source_text))
-    most_freq_words = get_most_frequent_words(loaded_text, get_args().words_count)
+    most_freq_words = get_most_frequent_words(loaded_text, words_count)
     for word, count in most_freq_words:
         print(word, count)
-
