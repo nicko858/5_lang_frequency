d7e981240686cebff22d5c82a286013d4139de06
nicko858 <ikar2021@hotmail.com>
2018-04-04 10:36:34 +0300

    Разбиение длинных строк

--- lang_frequency.py
+++ lang_frequency.py
@@ -15,8 +15,14 @@ def get_most_frequent_words(text, words_count):
 
 def get_args():
     script_usage = "lang_frequency.py <path to file> <words_count>"
-    parser = argparse.ArgumentParser(description="How to run lang_frequency.py:", usage=script_usage)
-    parser.add_argument("source_text", help="Specify the path to the source text file")
+    parser = argparse.ArgumentParser(
+        description="How to run lang_frequency.py:",
+        usage=script_usage
+    )
+    parser.add_argument(
+        "source_text",
+        help="Specify the path to the source text file"
+    )
     parser.add_argument(
         "words_count",
         type=int,
@@ -39,4 +45,4 @@ if __name__ == "__main__":
         exit("No such file or directory {}".format(source_text))
     most_freq_words = get_most_frequent_words(loaded_text, words_count)
     for word, count in most_freq_words:
-        print(word, count)
+        print (word, count)
